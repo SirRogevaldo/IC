97d3b0f093cd47c157214418188db0405de13b81
SirRogevaldo <93204200+SirRogevaldo@users.noreply.github.com>
2022-12-26 18:27:12 +0000

    Update testFcm.cpp

--- Project3/test/testFcm.cpp
+++ Project3/test/testFcm.cpp
@@ -7,13 +7,15 @@ int main(int argc, char** argv){
 
     map<string, map<char, int>> model;
     
-    if(argc != 2){
+    if(argc != 3){
         cout << "Error: Syntax" << endl;
         return 0; 
     }
 
     fcm f(3, 0.1);
     f.loadModel(model, argv[1]);
+
+    f.estimate(model,argv[2]);
     model.clear();
 
 }
