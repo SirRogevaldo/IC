70386ebe7c1b70550a0e5d90a10dc924902e5b64
Joaomg001 <joao.mendes.goncalves@ua.pt>
2023-01-09 03:59:41 +0000

    lang number of bits

--- Project3/src/lang.cpp
+++ Project3/src/lang.cpp
@@ -5,14 +5,15 @@ int main(int argc, char** argv){
     map<string, map<char, int>> model;
     
     if(argc != 3){
-        cout << "Error: Syntax: <Model> <Text> " << endl;
+        cout << "Error: Syntax: <Text> <Text> " << endl;
         return 0; 
     }
 
     fcm f(3, 0.1);
-    f.loadModel(model, argv[1]);
 
+    f.loadModel(model, argv[1]);
     f.estimate(model,argv[2]);
-    model.clear();
+    
+    cout << "Estimated number of bits to compress file: " << f.distance<< endl;
 
 }
